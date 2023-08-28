1f6d6a76c25d6624df78caaf5cffab5589b9982b
FarhalBasu <basu.farhal@gmail.com>
2023-08-28 23:16:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Tooltip from "./Tooltip";
 
 function App() {
+  // Declare a state variable called position and a function to update it called setPosition, both initialized to an empty string
+  const [position, setPosition] = useState("");
+
+  // Render the four buttons and the Tooltip component, passing the position state variable as a prop to the Tooltip component
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="buttons">
+        <button onClick={() => setPosition("top")}>Top</button>
+        <button onClick={() => setPosition("bottom")}>Bottom</button>
+        <button onClick={() => setPosition("left")}>Left</button>
+        <button onClick={() => setPosition("right")}>Right</button>
+      </div>
+      {/* Tooltip component with `position` prop passed in */}
+      <Tooltip position={position} />
     </div>
   );
 }
